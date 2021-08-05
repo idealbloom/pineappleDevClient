a1f3e8b075ca636f6e7610f6e818849ef262f21b
Minju <porpoise070@gmail.com>
2021-08-05 13:17:29 +0900

    git pull add

--- src/login/Findps.js
+++ src/login/Findps.js
@@ -3,13 +3,11 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import { style } from 'styled-system';
-import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
-  TextStyle1: {
+  box: {
     borderRadius: 8,
     backgroundColor: '#a5a8ae',
-    margin: 20,
     width: 240,
     height: 104,
     fontSize: 17,
@@ -18,6 +16,7 @@ const useStyles = makeStyles({
     letterSpacing: -0.11,
     textAlign: 'center',
     color: '#fafafa',
+    margin: 40,
   },
   TextStyle2: {
     fontSize: 10,
@@ -35,10 +34,12 @@ const useStyles = makeStyles({
     lineHeight: 22,
     letterSpacing: -0.11,
   },
-  View: {
-    margin: 20,
-    width: 350,
-    height: 440,
+
+  bg: {
+    justifyContent: 'center',
+    width: '100%',
+    height: 844,
+    textAlign: 'center',
     borderRadius: 20,
     backgroundColor: 'rgba(237, 237, 237, 0.35)',
     shadowColor: 'rgba(133, 140, 146, 0.2)',
@@ -66,18 +67,19 @@ export default function Findps() {
   const classes = useStyles();
 
   return (
-    <Box style={style.View}>
+    <Box style={style.bg}>
       <Box>
-        <span> 아이디는 아래와 같습니다. </span>
+        <span>{'  '} 아이디는 아래와 같습니다. </span>
       </Box>
-      <Box className={classes.TextStyle1}>
-        <TextField className={classes.TextStyle2}>user@gmail.com</TextField>
+      <Box className={classes.box}>
+        <text> user@gmail.com </text>
       </Box>
       <Box>
-        <TextField className={classes.TextStyle3}>
+        <span className={classes.TextStyle3}>
           비밀번호를 분실하셨다면 다음을 눌러 초기화를 진행합니다.
-        </TextField>
+        </span>
       </Box>
+
       <Button className={classes.root}> 로그인 </Button>
       <Button className={classes.root}> 다음 </Button>
     </Box>
